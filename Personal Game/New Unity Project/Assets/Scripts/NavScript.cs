e5748f130e5870998ad1a436c712031596c0f562
Ariel <kailichen99@gmail.com>
2019-10-11 11:44:30 +1300

    Uh Oh Stinky

---
+++ Personal Game/New Unity Project/Assets/Scripts/NavScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavScript : MonoBehaviour
+{
+    public GameObject[] navpoints;
+    NavMeshAgent nav;
+    public float dtd,dth;
+    int currenttarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        nextnode();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dtd = Vector3.Distance(navpoints[currenttarget].transform.position, transform.position);
+        
+        if(dtd < dth)
+        {
+            nextnode();
+        }
+    }
+
+    void nextnode()
+    {
+        currenttarget = Random.Range(0, navpoints.Length);
+        nav.SetDestination(navpoints[currenttarget].transform.position);
+    }
+}
